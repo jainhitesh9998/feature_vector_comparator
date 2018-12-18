b988a8085b551120262667b33ff4487b5d680194
Raghavaraman <raghavaraman.s@gmail.com>
2018-12-18 16:05:50 +0530

    enum added

--- comparator/comparator.py
+++ comparator/comparator.py
@@ -2,21 +2,44 @@ from keras.layers import Input, Dense, concatenate, Lambda
 from keras.models import Model
 from keras import backend as K
 from keras.regularizers import l2
+from enum import Enum
+
+# def enum(**enums):
+#     return type('Enum', (), enums)
 
 
-def enum(**enums):
-    return type('Enum', (), enums)
+def euclidian_distance():
+    return Lambda(lambda tensors: K.abs(K.square(tensors[0]) - K.square(tensors[1])))
+
+def cosine_distance():
+    return Lambda(
+        lambda tensors: K.abs((K.dot(tensors[0], tensors[1])) / (K.square(tensors[0]) * K.square(tensors[1]))))
+
+def absolute_distance():
+    return Lambda(lambda tensors: K.abs(tensors[0]- tensors[1]))
+
+
+class Distance(Enum):
+    ABSOLUTE = absolute_distance
+    COSINE = cosine_distance
+    EUCLIDIAN = euclidian_distance
 
 
 class Comparator(object):
 
-    def __init__(self, input_size=128, hidden_layers = 2 , loss=0):
+    def __init__(self, input_size=128, hidden_layers = 2 , distance = Distance.ABSOLUTE, loss=0):
+
+        # self.__input_reference = self.__input_layer('reference_layer')
+        # self.__input_test = self.__input_layer('test_layer')
+        # assert (isinstance(distance, Enum))
         self.__model = None
         self.__input_size = input_size
         self.__input_shape = (None, self.__input_size)
         self.__hidden_layers = hidden_layers
         self.__weight_decay = 0.0005
         self.__weight_init = "he_normal"
+        print(distance)
+        self.__comparator_layer = distance()
 
     def __input_layer(self, name='input'):
         return Input(shape=self.__input_shape, name=name)
@@ -49,8 +72,9 @@ class Comparator(object):
             test_layer = self.__fully_connected_layer(test_layer, neurons=(self.__hidden_layers - l) * self.__input_size,
                                          name='ref_test_layer' + str(l+1),
                                          activation='relu')
-        comparator_layer = Lambda(lambda tensors: K.abs(K.square(tensors[0]) - K.square(tensors[1])))
-        vector_distance = comparator_layer([ref_layer, test_layer])
+        # comparator_layer = Lambda(lambda tensors: K.abs(K.square(tensors[0]) - K.square(tensors[1])))
+        vector_distance = self.__comparator_layer([ref_layer, test_layer])
         prediction = Dense(units=1, activation='sigmoid', name='output')(vector_distance)
         self.__model = Model(inputs=[input_reference, input_test], outputs=prediction)
         return self.get_model()
+
--- model_test.py
+++ model_test.py
@@ -1,8 +1,17 @@
 from keras.layers import Input, Dense, concatenate, Lambda
 from keras.models import Model
 from keras import backend as K
-
+from comparator.comparator import Distance
 from comparator.comparator import Comparator
+from keras.applications.resnet50 import ResNet50
+# reference_resnet = ResNet50(include_top=True, weights='imagenet', input_tensor=None, input_shape=None, pooling=None, classes=1000)
+# test_resnet = ResNet50(include_top=True, weights='imagenet', input_tensor=None, input_shape=None, pooling=None, classes=1000)
+# test_resnet.summary()
 
-model = Comparator( input_size=256)()
+# print(type(test_resnet.layers[-2][0]))
+# print(test_resnet.layers[-3].shape)
+# model = Comparator(input_size=test_resnet.layers[-2].output.shape[1], hidden_layers=2, distance=Distance.ABSOLUTE, \
+#     reference_Model= reference_resnet, test_model=None)()
+
+model = Comparator(input_size=128, hidden_layers=2, distance=Distance.EUCLIDIAN)()
 model.summary()
